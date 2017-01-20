c9183e4097e693b78439fdaf64cdf60f63dfe425
Jufebrown <jufebrown@gmail.com>
2017-01-20 10:20:06 -0600

    added controller and factory for main

--- app/app.js
+++ app/app.js
@@ -6,7 +6,29 @@ app.config(($routeProvider, $locationProvider) => {
   .when('/', {
     controller: 'MainCtrl',
     templateUrl: 'partials/main.html'
+  })
   .otherwise({
     redirectTo: '/'
   })
+})
+
+app.controller('MainCtrl', function($scope, mainFactory) {
+  mainFactory.getList()
+  .then((val) => {
+    console.log('val from listctrl', val)
+    $scope.list = val.list
+  })
+})
+
+
+app.factory('mainFactory', function($http){
+  return {
+    getList : () => {
+      return $http.get('mushroom.json')
+      .then((value) => {
+        console.log(value)
+        return value.data
+      })
+    }
+  };
 })
