169f05d76b6ca96e11f26601efb672163d791d13
Jufebrown <jufebrown@gmail.com>
2017-01-20 09:48:46 -0600

    declared app, added router

---
+++ app/app.js
@@ -0,0 +1,12 @@
+const app = angular.module('mushroomApp', ['ngRoute'])
+
+app.config(($routeProvider, $locationProvider) => {
+  $locationProvider.hashPrefix('')
+  $routeProvider
+  .when('/', {
+    controller: 'MainCtrl',
+    templateUrl: 'partials/main.html'
+  .otherwise({
+    redirectTo: '/'
+  })
+})
